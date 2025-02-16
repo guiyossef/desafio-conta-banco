9cb890e907eb673b822131cbce9b98ac91d5714b
guiyossef <1yossefcontato@gmail.com>
2025-02-16 18:20:35 -0300

    git commit -m desafio dio ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int Numero;
+        double Saldo;
+        
+            String Agencia = "";
+            String NomeCliente = "";
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite o nome do cliente:");
+            NomeCliente =  scanner.nextLine();
+        
+            System.out.println("Digite o número da agencia: ");
+            Agencia = scanner.nextLine();
+        
+            System.out.println("digite o número da conta:");
+            Numero = scanner.nextInt();
+        
+            System.out.println("Digitem o valor do saldo:");
+            Saldo = scanner.nextDouble();
+        
+            System.out.println("Olá " + NomeCliente + " obrigado por criar uma conta em nosso banco,"
+                + " sua agência é " + Agencia + ", conta " + Numero +" e seu saldo " + Saldo + " já está disponível para saque ");
+
+            scanner.close();        
     }
 }
